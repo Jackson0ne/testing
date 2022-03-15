9bd5d69079622f1ae5dbc7afec385409687889a4
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-03-15 20:17:00 +0000

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,4 +1,4 @@
-const scriptver = "0.4";
+GLOBAL.scriptver="0.5";
 
 document.getElementById("testdiv").style.background = "green";
 document.getElementById("testdiv").innerHTML = "IT'S WORKING! 😀";
