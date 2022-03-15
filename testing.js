d987f1fc0045679021d57d32f14e123fd05819ba
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-03-15 14:11:27 +0000

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,4 +1,4 @@
 const scriptver = "0.1";
 
 document.getElementById("testdiv").style.background = "green";
-document.getElementById("testdiv").innerHTML = "Testing again!";
+document.getElementById("testdiv").innerHTML = "Newest!";
