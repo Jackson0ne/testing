6f1b6c57339ac1ae54a7a9feab7a2e8b0f83142e
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-03-15 10:47:53 +0000

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,4 +1,4 @@
 const scriptver = "0.1";
 
 document.getElementById("testdiv").style.background = "green";
-document.getElementById("testdiv").innerHTML = "Success! (Latest Version)";
+document.getElementById("testdiv").innerHTML = "Success! (Updated)";
