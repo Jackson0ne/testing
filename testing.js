15bf3a361f571eaf770ae6bbe5890e09a763d6a5
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-03-17 11:29:25 +0000

    Delete testing.js

--- testing.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("testdiv").style.background = "green"
-document.getElementById("testdiv").innerHTML = "Newest Testing!"
-
-function TestFunc() {
-    document.getElementById("testdiv").style.width = "50px";
-}
