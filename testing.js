82823d986a162955be2c3d2d9b68954af0b99fb8
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-03-17 11:28:34 +0000

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,6 @@
+document.getElementById("testdiv").style.background = "green"
+document.getElementById("testdiv").innerHTML = "Newest Testing!"
+
+function TestFunc() {
+    document.getElementById("testdiv").style.width = "50px";
+}
