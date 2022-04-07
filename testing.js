e6ec5f9eb62c41cc8efe1e2c3311764ad5f46095
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-04-07 10:53:40 +0100

    Delete testing.js

--- testing.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("testdiv").innerHTML = "TEST!";
-
-function TestFunc() {
-    document.getElementById("testdiv").style.animation = "hue 10s linear alternate infinite forwards";
-}
-
-TestFunc();
