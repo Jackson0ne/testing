7603d2e4740493a2993f4f320b158b8bc3fe8874
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-04-07 15:25:33 +0100

    Update newnew.js

--- newnew.js
+++ newnew.js
@@ -1,2 +1,5 @@
-document.getElementById("test").innerHTML = "This is the sixth test"
+document.getElementByTagName("body").style.background = "blueviolet"
+
+document.getElementById("test").innerHTML = "This is the seventh test"
+document.getElementById("test").style.color = "white"
 document.getElementById("test").style.fontSize = "20px"
