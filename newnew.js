ebcb71170f9b6490f4ecda74df8a6bba4f1d1eba
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-04-07 16:11:02 +0100

    Delete newnew.js

--- newnew.js
+++
@@ -1,5 +0,0 @@
-document.body.style.backgroundColor = "blueviolet"
-
-document.getElementById("test").innerHTML = "This is the seventh test"
-document.getElementById("test").style.color = "white"
-document.getElementById("test").style.fontSize = "20px"
