e6e2ceb7fb29c33b94860be7cd891cd7f4520e73
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-05-09 16:44:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const startapp = () => {
 
     function createWindow() {
         win = new BrowserWindow({
-            width: 300,
-            height: 200,
+            width: 800,
+            height: 600,
             webPreferences: {
                 nodeIntegration: true,
                 contextIsolation: false
